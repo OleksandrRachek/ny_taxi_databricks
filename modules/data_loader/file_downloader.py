0482138e39c810a469296e35541aeca9e90307b4
OleksandrRachek <122453299+OleksandrRachek@users.noreply.github.com>
2025-10-07 13:28:12 +0300

    Add files via upload

--- modules/data_loader/file_downloader.py
+++ modules/data_loader/file_downloader.py
@@ -10,6 +10,8 @@ def download_file (url: str, dir_path: str, local_path: str):
     """
     os.makedirs(os.path.dirname(dir_path), exist_ok= True)
 
+    print ('folder is created')
+
 
     with urllib.request.urlopen(url) as response, open (local_path, 'wb') as out_file:
         shutil.copyfileobj(response, out_file)
