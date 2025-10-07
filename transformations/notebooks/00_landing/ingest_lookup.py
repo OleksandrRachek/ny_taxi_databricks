bfa4f55639833e1b18c23b8dca214ef24b34780c
OleksandrRachek <122453299+OleksandrRachek@users.noreply.github.com>
2025-10-07 13:10:02 +0300

    Add files via upload

--- transformations/notebooks/00_landing/ingest_lookup.py
+++ transformations/notebooks/00_landing/ingest_lookup.py
@@ -1,28 +1,42 @@
 # Databricks notebook source
 import urllib.request
 import os
+import sys
 import shutil
+
+from modules.data_loader.file_downloader import download_file 
+
+#Go 2 levels up to reach the project root
+
+project_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))
+
+
+if project_root not in sys.path:
+    sys.path.append(project_root)
+
+
 
 try:
     # Construct the URL for the Parquet file corresponding to this month
     url = "https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_lookup.csv"
 
+
+    #Define and create the local directory for this date's data
+    dir_path = f"/Volumes/nyctaxi/00_landing/data_sources/lookup"
+
+    #Define the full path for the downloaded file
+    local_path = f"{dir_path}/taxi_zone_lookup.csv"
+
+    #Download the file 
+    download_file(url=url, dir_path=dir_path, local_path=local_path)
+
     # Open a connection and stream the remote file
     response = urllib.request.urlopen(url)
 
-    # Define and create the local directory for this date's data
-    dir_path = f"/Volumes/nyctaxi/00_landing/data_sources/lookup"
-    os.makedirs(dir_path, exist_ok=True)
-
-    # Define the full path for the downloaded file
-    local_path = f"{dir_path}/taxi_zone_lookup.csv"
-
-    # Save the streamed content to the local file in binary mode
-    with open(local_path, 'wb') as f:
-        shutil.copyfileobj(response, f)  # Copy data from response to file
     
     dbutils.jobs.taskValues.set(key="continue_downstream", value="yes")
     print("File succesfully uploaded")
+
 except Exception as e:
     dbutils.jobs.taskValues.set(key="continue_downstream", value="no")
     print(f"File download failed: {str(e)}")
